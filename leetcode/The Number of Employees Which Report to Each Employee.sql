7559de80cb29061d8ca918bb4a987602406175ec
Shifa <bsse1301@iit.du.ac.bd>
2024-12-26 15:53:12 +0600

    Create The Number of Employees Which Report to Each Employee.sql

---
+++ leetcode/The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,2 @@
+
+select a.employee_id, a.name, count(b.employee_id) as reports_count, round(avg(b.age)) as average_age from Employees as a inner join Employees as b on a.employee_id=b.reports_to group by a.employee_id, a.name order by a.employee_id;
