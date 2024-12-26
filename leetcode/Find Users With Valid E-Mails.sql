f2acaf3ae113b848ff80c2d0c1d579545820a588
Shifa <bsse1301@iit.du.ac.bd>
2024-12-26 22:31:16 +0600

    Update Find Users With Valid E-Mails.sql

--- leetcode/Find Users With Valid E-Mails.sql
+++ leetcode/Find Users With Valid E-Mails.sql
@@ -2,3 +2,7 @@ right(mail,13)='@leetcode.com' and
 upper(left(mail,1)) between 'A' and 'Y' 
 and left(mail,length(mail)-13) REGEXP '^[a-zA-Z0-9_.-]+$'
 ;
+
+right ans: 
+select * from Users where 
+mail regexp '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[\.]com$';
